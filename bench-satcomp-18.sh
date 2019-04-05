75caa44c00f20996ddd9e967b670b9e1d1ccce9f
Simon Cruanes <simon.cruanes.2007@m4x.org>
2019-04-05 23:22:46 +0200

    scripts for benchs

--- bench-satcomp-18.sh
+++ bench-satcomp-18.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 if [ -n $J ] ; then J="17" ; fi
 TEST_OPTS="-j $J --progress"
 DATE="`date +%FT%H:%M`"
@@ -7,7 +6,7 @@ DATE="`date +%FT%H:%M`"
 mkdir -p snapshots
 logitest run --meta=`git rev-parse HEAD` \
   --summary snapshots/bench-satrace06-$DATE.txt \
-  --csv snapshots/bench-satrace06-$DATE.csv \
-  -c benchs/conf.toml --profile=bench-satrace-06 $TEST_OPTS
+  --csv snapshots/bench-satcomp18-$DATE.csv \
+  -c benchs/conf.toml --profile=bench-satcomp-18 $TEST_OPTS
 
 
