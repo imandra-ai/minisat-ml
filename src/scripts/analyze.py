fd834979d36268997dfb9e722829f7e6c717a520
Simon Cruanes <simon.cruanes.2007@m4x.org>
2019-04-16 17:09:42 -0500

    refactor(script): modify analyze script so it works on mfleury's results

--- src/scripts/analyze.py
+++ src/scripts/analyze.py
@@ -7,17 +7,25 @@ def read_csv(f):
         content = fd.readlines()
     return list(csv.DictReader(content)) 
 
-def analyze_file(f, potential_errors=False, plot=None):
+def analyze_file(f, potential_errors=False, plot=None, mfleury=False):
     """Analyze result file {f} (which should be a .csv file).
     
     Print per-solver analysis, and errors which happen quickly (true errors, not timeouts).
     If `plot` is provided, call `mkplot.py` to produce a nice plot of the results.
+    If `mfleury` is true, use the alternative format provided by Mathias Fleury
     """
     print(f"## analyze `{f}`")
     table = read_csv(f)
     print(f"read {len(table)} records")
     if not table: return
-    provers = [x for x in table[0].keys() if ".time" not in x and x != "problem"]
+    if mfleury:
+        time_suffix = '_overall_time'
+        res_suffix = '_result'
+        provers = [x.split('_result')[0] for x in table[0].keys() if "_result" in x]
+    else:
+        time_suffix = '.time'
+        res_suffix = ''
+        provers = [x for x in table[0].keys() if ".time" not in x and x != "problem"]
     print(f"provers: {provers}")
     sat = {}
     unsat = {}
@@ -28,19 +36,19 @@ def analyze_file(f, potential_errors=False, plot=None):
         quick_errors = []
     for row in table:
         for prover in provers:
-            res = row[prover]
-            time = float(row[prover + '.time'])
+            res = row[prover + res_suffix]
+            time = row[prover + time_suffix]
+            time = float(time) if time != 'NULL' else 0
             if res == 'unsat':
                 unsat[prover] = 1 + unsat.get(prover, 0)
                 total_time[prover] = time + total_time.get(prover,0)
             elif res == 'sat':
                 sat[prover] = 1 + sat.get(prover, 0)
                 total_time[prover] = time + total_time.get(prover,0)
-            elif res == 'unknown':
+            elif res == 'unknown' or res == 'timeout' or res == 'NULL':
                 unknown[prover] = 1 + unknown.get(prover, 0)
             elif res == 'error':
                 error[prover] = 1 + error.get(prover, 0)
-                time = float(row[prover + '.time'])
                 if potential_errors and time < 5:
                     quick_errors.append((prover, row['problem'], time))
             else:
@@ -52,8 +60,8 @@ def analyze_file(f, potential_errors=False, plot=None):
             f" | solved {n:6}" \
             f" | unknown {unknown.get(prover,0):6}" \
             f" | error {error.get(prover,0):6}" \
-            f" | total-time {total_time.get(prover,0):{12.5}}s" \
-            f" | avg-time {0 if n==0 else total_time.get(prover,0)/n:{6.3}}s")
+            f" | total-time {total_time.get(prover,0):14.3f}s" \
+            f" | avg-time {0 if n==0 else total_time.get(prover,0)/n:8.3f}s")
 
     if potential_errors:
         for (prover,filename,time) in quick_errors:
@@ -70,9 +78,10 @@ def analyze_file(f, potential_errors=False, plot=None):
                 json_files.append(filename)
                 stats = {}
                 for row in table:
-                    res = row[prover]
+                    res = row[prover + res_suffix]
                     ok = (res == 'unsat' or res=='sat')
-                    time = float(row[prover + '.time'])
+                    time = row[prover + time_suffix]
+                    time = float(time) if time != 'NULL' else 0
                     stats[row['problem']] = {'status': ok, 'rtime': time}
                 j = { 'preamble': {'program': prover}, 'stats': stats, }
                 with open(filename, 'w') as out:
@@ -81,9 +90,9 @@ def analyze_file(f, potential_errors=False, plot=None):
             subprocess.call(["mkplot.py", "--save-to="+plot, "-b", "png"] + json_files)
 
 
-def main(files, potential_errors=False, plot=False) -> ():
+def main(files, **kwargs) -> ():
     for f in files:
-        analyze_file(f, potential_errors=potential_errors, plot=plot)
+        analyze_file(f, **kwargs)
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser('analyze result files')
@@ -91,5 +100,8 @@ if __name__ == "__main__":
     p.add_argument('--errors', dest='potential_errors', \
             action='store_true', help='detect potential errors')
     p.add_argument('--plot', dest='plot', help='produce a plot')
+    p.add_argument('--mfleury', dest='mfleury', action='store_true',
+            help="use mathias fleury's input format")
     args = p.parse_args()
-    main(files=args.files, potential_errors=args.potential_errors, plot=args.plot)
+    main(files=args.files, potential_errors=args.potential_errors,
+            plot=args.plot, mfleury=args.mfleury)
